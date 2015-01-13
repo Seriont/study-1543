cf44035d7019a3db1b71944d74d22c84a7065ccb
Nikolay Skachkov <nikolajskachkov@gmail.com>
2015-01-13 19:11:42 +0300

    Добавлены исправления в класс генерации. Исправлены классы Time, Generator а так же изменены некоторые функции

--- modeling/modeling.py
+++ modeling/modeling.py
@@ -2,13 +2,16 @@
 
 import random
 
-from models.models import Launching, Car
+from models.models import Launching, Car, Log, Time
 from common.functions import latest_launching_id, get_road
 
 
-class Generator():
+class Generator(Log):
     """
     Данный класс используется при конфигурации нового запуска и только тогда
+
+    Все функции random можно будет сделать более умными, когда будет сделана
+    основная часть проекта
     """
 
     def __generate_car(self, car_id):
@@ -27,12 +30,34 @@ class Generator():
     ):
         road = get_road(road_id)
         cars_list = []
+        time_start = Time(0)
         for car_id in range(number_of_cars):
-            cars_list.append(self.__generate_car(car_id))
+            cars_list.append([
+                self.__generate_car(car_id),
+                time_start
+            ])
+            time_start.increase(1+random.random()%20)
         launching_id = latest_launching_id() + 1
         launching = Launching(
             launching_id=launching_id,
             road=road,
             cars_list=cars_list,
         )
-        return launching
+        self.write_in_log(launching)
+
+        return launching_id
+
+
+class Modeling(Log):
+    '''
+    Данный класс осуществляет движение машин и запись изменений в лог,
+    при visualize = True, происходит визуализация движения, а запись в лог
+    отключается.
+
+    get - это функция доставания из лога чего-либо по его id и, возможно,
+    типу объекта
+    '''
+
+    def __init__(self, launching_id, visualize=False):
+        self.launching = self.get(launching_id)
+
